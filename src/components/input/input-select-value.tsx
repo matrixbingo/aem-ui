fcde3e42829285c9601c75fbdb291b14ab36ca71
Liang.wang <maoshiba2006@126.com>
2022-01-06 17:31:57 +0800

    Create input-select-value.tsx
    update

---
+++ src/components/input/input-select-value.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { Input, InputProps, Select } from 'antd';
+import { DataUtil } from 'common-toolkits';
+import  SelectSingle  from '../select/select-single';
+type InputSelectValueValue = string | undefined | null;
+
+export interface InputSelectValueProps extends Omit<InputProps, 'value' | 'onChange'> {
+  value: string;
+  onChange: (value: InputSelectValueValue) => void;
+  void: InputSelectValueValue;
+}
+
+const options = [
+  { id: 0, name: '无值' },
+  { id: 1, name: '有值' },
+];
+
+/**
+ * 如果选无值则清空 Type 0：无值 1：有值
+ */
+const InputSelectValue = (props: InputSelectValueProps) => {
+  const { value: inputValue, onChange: inputOnChange, void: voidValue, ...restProps } = props;
+  const _type = DataUtil.unknown.isVoid(inputValue) ? 0 : 1;
+  const [type, setType] = useState<number>(_type);
+  const [value, setValue] = useState<string>(inputValue);
+
+  useEffect(() => {
+    if (type === 0) {
+      inputOnChange(voidValue);
+      setValue('');
+    } else if (!value) {
+      inputOnChange('');
+    }
+  }, [type]);
+
+  const onChange = (e) => {
+    const { value: _value } = e.target;
+    setValue(_value);
+    inputOnChange(_value);
+  };
+
+  return (
+    <Input
+      {...restProps}
+      value={value}
+      disabled={type === 0}
+      onChange={onChange}
+      addonBefore={<SelectSingle<number> options={options} value={type} onSelect={(v) => setType(Number(v))} style={{ width: 70 }} />}
+    />
+  );
+};
+
+InputSelectValue.defaultProps = {
+  value: '',
+  onChange: (v) => {},
+  void: undefined,
+};
+
+export default InputSelectValue;
