1ceb491aed09b7a3c90d6da98fc225c648caab8e
Liang.wang <maoshiba2006@126.com>
2022-01-10 13:54:09 +0800

    Update mask-close-modal.tsx

--- src/components/modal/mask-close-modal.tsx
+++ src/components/modal/mask-close-modal.tsx
@@ -2,21 +2,34 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { Modal, ModalProps } from 'antd';
 
-export interface MaskCloseModalProps extends Omit<ModalProps, 'width' | 'maskClosable' | 'bodyStyle'> {
+type sizeType = 'small' | 'normal' | 'large';
+
+export interface MaskCloseModalProps extends Omit<ModalProps, 'width' | 'maskClosable' | 'bodyStyle' | 'size'> {
+  size?: sizeType;
   [K: string]: any;
 }
+
+const getWidth = (size: sizeType) => {
+  switch (size) {
+    case 'small': return 400;
+    case 'normal': return 800;
+    case 'large': return 1200;
+    default: return 1200;
+  }
+};
 
 /**
  * 常用Modal设置
  * @param param0
  * @returns
  */
-const MaskCloseModal: FC<PropsWithChildren<MaskCloseModalProps>> = ({ children, ...rest }) => {
-  return <Modal {...rest}>{children}</Modal>;
+const MaskCloseModal: FC<PropsWithChildren<MaskCloseModalProps>> = ({ children, size, ...rest }) => {
+  const width = getWidth(size ?? 'large');
+  return <Modal width={width} {...rest}>{children}</Modal>;
 };
 
 MaskCloseModal.defaultProps = {
-  width: 1200,
+  size: 'large',
   maskClosable: false,
   bodyStyle: { margin: '10px 30px 0px 30px' },
 };
