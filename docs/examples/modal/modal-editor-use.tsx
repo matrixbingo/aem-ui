4ccf80b3f67af436879729430ce3acb2d0e5a60d
Liang.wang <maoshiba2006@126.com>
2022-01-10 15:35:33 +0800

    Update modal-editor-use.tsx

--- docs/examples/modal/modal-editor-use.tsx
+++ docs/examples/modal/modal-editor-use.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import { ModalEditor } from 'aem-ui';
+import { delay } from 'lodash';
 
 const Demo: React.FC = () => {
 
+  const onSubmit = (setVisible, setLoading) => {
+    delay(()=> {
+      setLoading(false);
+     // setVisible(false);
+    }, 3000);
+  }
+
   return (
     <>
-      <ModalEditor button={{ children:"编辑" }}  title="ModalEditor">
+      <ModalEditor button={{ children:"编辑1" }}  title="ModalEditor" onSubmit={onSubmit}>
+        do something...
+      </ModalEditor>
+
+      <br />
+      <ModalEditor button={{ children:"编辑1" }}  title="ModalEditor" onOk={()=>{}}>
         do something...
       </ModalEditor>
     </>
