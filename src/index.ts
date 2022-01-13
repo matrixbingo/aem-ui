453804811e4aa0e6de2285beee9f495bb8794534
Liang.wang <maoshiba2006@126.com>
2022-01-13 20:15:30 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
+// button 
+export { default as ButtonLoading } from './components/button/button-loading';
+import type { ButtonLoadingProps } from './components/button/button-loading';
+// input
+export { default as InputAddonBefore } from './components/input/input-addon-before';
+// import type { default as InputAddonBefore } from './components/input/input-addon-before';
+
+export { default as InputDefaultClear } from './components/input/input-default-clear';
+export { default as InputStringNumber } from './components/input/input-string-number';
+export { default as InputRange } from './components/input/input-range';
+export { default as InputRenderCustomer } from './components/input/input-render-customer';
+export { default as InputSelectValue } from './components/input/input-select-value';
+
 // card
 export { default as CollapsibleCard } from './components/card/collapsible-Card';
+
 // code-editor
 export { default as CodeEditor } from './components/code-editor/code-editor';
 export { default as PopoverCode } from './components/code-editor/popover-code/popover-code';
@@ -11,13 +25,6 @@ export { default as RangePickerFormat } from './components/date-picker/range-pic
 // descriptions
 export { default as DescriptionsPlus } from './components/descriptions/descriptions-plus';
 
-// input
-export { default as InputAddonBefore } from './components/input/input-addon-before';
-export { default as InputDefaultClear } from './components/input/input-default-clear';
-export { default as InputStringNumber } from './components/input/input-string-number';
-export { default as InputRange } from './components/input/input-range';
-export { default as InputRenderCustomer } from './components/input/input-render-customer';
-export { default as InputSelectValue } from './components/input/input-select-value';
 
 // label
 export { default as DescriptionsLabel } from './components/label/descriptions-label';
@@ -53,10 +60,9 @@ export { default as TextAreaJson } from './components/textarea-json/textarea-jso
 export { default as View } from './components/view/view';
 export { default as ViewContainer } from './components/view/view-container';
 
-// button 
-export { default as ButtonLoading } from './components/button/button-loading';
 
 import { createBaseButtons, CreateButtonsProps, createBaseTabs, CreateBaseTabsProps, createStart, CreateStartProps } from './components/util/create-ant';
 
 export { createBaseButtons, createBaseTabs, createStart };
+export type { ButtonLoadingProps, };
 export type { CreateButtonsProps, CreateBaseTabsProps, CreateStartProps };
