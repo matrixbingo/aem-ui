bb45e4cb2adf51fc80fd1ad323760824d2d8f914
Matrix <maoshiba2006@126.com>
2022-01-23 10:54:02 +0800

    新增code-editor-card

--- packages/aem-ui/src/components/label/omit-tip-label.tsx
+++ packages/aem-ui/src/components/label/omit-tip-label.tsx
@@ -2,10 +2,15 @@ import { Tooltip } from 'antd';
 import { StringUtil } from 'common-toolkits';
 import React, { FC } from 'react';
 
+export interface OmitTipLabelProps {
+  title: string;
+  limit: number;
+}
+
 /**
  * 表格列表等缩略显示
  */
-const OmitTipLabel: FC<{ title: string; limit: number }> = ({ title = '', limit = 10 }) => {
+const OmitTipLabel: FC<OmitTipLabelProps> = ({ title = '', limit = 10 }) => {
   if (title?.length < limit) {
     return <>{title}</>;
   }
